af7da44d06c97b3cde2d11cf252900426c3b2292
Igor Bińkowski <Igor30522@gmail.com>
2023-02-12 22:03:15 +0100

    Commit 2

--- app/src/main/java/com/example/sprawdzaniehasla/MainActivity.kt
+++ app/src/main/java/com/example/sprawdzaniehasla/MainActivity.kt
@@ -5,10 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
-import android.widget.ImageView
-import android.widget.TextView
 import com.google.android.material.textfield.TextInputEditText
-
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,29 +13,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.Sprawdź).setOnClickListener {
-            var password = findViewById<TextInputEditText>(R.id.Hasło).text.toString()
-            var potwierdź = findViewById<TextInputEditText>(R.id.Potwierdź).text.toString()
-            var znak = "-?+(\\.[%!_$#&*-]+)?".toRegex()
-            if (password == potwierdź) {
+            val password = findViewById<TextInputEditText>(R.id.Hasło).text.toString()
+            val potwierdz = findViewById<TextInputEditText>(R.id.Potwierdz).text.toString()
+            val znak = "-?+(\\.[%!_$#&*-]+)?".toRegex()
+
+            if (password == potwierdz) {
                 findViewById<CheckBox>(R.id.checkBox).isChecked = password.any()
             } else {
-                findViewById<CheckBox>(R.id.checkBox).isChecked =false
-                if (password.any { it.isUpperCase() } && potwierdź.any { it.isUpperCase() }) {
+                findViewById<CheckBox>(R.id.checkBox).isChecked =false}
+
+                if (password.any { it.isUpperCase() } && potwierdz.any { it.isUpperCase() }) {
                     findViewById<CheckBox>(R.id.checkBox2).isChecked = password.any()
                 } else {
                     findViewById<CheckBox>(R.id.checkBox2).isChecked = false
                 }
-                if (password.any { it.isLowerCase() } && potwierdź.any { it.isLowerCase() }) {
+                if (password.any { it.isLowerCase() } && potwierdz.any { it.isLowerCase() }) {
                     findViewById<CheckBox>(R.id.checkBox3).isChecked = password.any()
                 } else {
                     findViewById<CheckBox>(R.id.checkBox3).isChecked = false
                 }
-                if (password.any { it.isUpperCase() } && potwierdź.any { it.isUpperCase() }) {
+                if (password.any { it.isDigit() } && potwierdz.any { it.isDigit() }) {
                     findViewById<CheckBox>(R.id.checkBox4).isChecked = password.any()
                 } else {
                     findViewById<CheckBox>(R.id.checkBox4).isChecked = false
                 }
-                if (password.any { it.isUpperCase() } && potwierdź.any { it.isUpperCase() }) {
+                if (znak.containsMatchIn(password)) {
                     findViewById<CheckBox>(R.id.checkBox5).isChecked = password.any()
                 } else {
                     findViewById<CheckBox>(R.id.checkBox5).isChecked = false
@@ -46,4 +45,3 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-}
